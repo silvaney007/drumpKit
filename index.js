c8ad8f638f55bcd68343f4a2816c7589a5c3a909
dsrules007 <sousilvaney@gmail.com>
2020-11-09 01:26:52 +0000

    add music and animations (it's done)

--- index.js
+++ index.js
@@ -1,12 +1,75 @@
-document.querySelector('button').addEventListener('click', handleClick);
+var dumLength = document.querySelectorAll('.drum').length;
 
-function handleClick() {
-    document.querySelectorAll.map(
-        button => button.addEventListener('click', playSound)
-    )
+for (i = 0; i < dumLength; i++) {
+
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+
+        var buttonInnerHTML = this.innerHTML;
+        playSound(buttonInnerHTML);
+        buttonShandow(buttonInnerHTML);
+
+    })
 }
 
-function playSound() {
-    var audio = new Audio('sounds/tom-1.mp3')
-    audio.play();
+document.addEventListener('keypress', function (event) {
+
+    playSound(event.key);
+    buttonShandow(event.key);
+
+})
+
+
+function playSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+
+        default:
+            console.log(key);
+
+    }
+}
+
+function buttonShandow(event) {
+
+    var currentKey = document.querySelector('.' + event);
+
+    currentKey.classList.add('pressed');
+
+    setTimeout(function () {
+        currentKey.classList.remove('pressed');
+    }, 100)
 }
